cb0c935024fc759e1808c5aa9c1e16b641ce43de
lune07 <blswnita70@gmail.com>
2023-10-23 20:32:16 +0530

    files added

---
+++ get_pet_labels.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
+#                                                                             
+# PROGRAMMER: Archita
+# DATE CREATED: 09/07/23                                 
+# REVISED DATE: 09/07/23
+# PURPOSE: Create the function get_pet_labels that creates the pet labels from 
+#          the image's filename. This function inputs: 
+#           - The Image Folder as image_dir within get_pet_labels function and 
+#             as in_arg.dir for the function call within the main function. 
+#          This function creates and returns the results dictionary as results_dic
+#          within get_pet_labels function and as results within main. 
+#          The results_dic dictionary has a 'key' that's the image filename and
+#          a 'value' that's a list. This list will contain the following item
+#          at index 0 : pet image label (string).
+#
+##
+# Imports python modules
+import os
+from os import listdir
+
+# TODO 2: Define get_pet_labels function below please be certain to replace None
+#       in the return statement with results_dic dictionary that you create 
+#       with this function
+# 
+
+'''print("\nPrint names")
+for i in range(0,10,1):
+    print("{:2d} file: {:>25}".format(i+1, filename_list[i]))
+   Creates a dictionary of pet labels (results_dic) based upon the filenames 
+    of the image files. These pet image labels are used to check the accuracy 
+    of the labels that are returned by the classifier function, since the 
+    filenames
+  of the images contain the true identity of the pet in the image.
+    Be sure to format the pet labels so that they are in all lower case letters
+    and with leading and trailing whitespace characters stripped from them.
+    (ex. filename = 'Boston_terrier_02259.jpg' Pet label = 'boston terrier')
+    Parameters:
+     image_dir - The (full) path to the folder of images that are to be
+                 classified by the classifier function (string)
+    Returns:
+      results_dic - Dictionary with 'key' as image filename and 'value' as a 
+      List. The list contains for following item:
+         index 0 = pet image label (string)
+'''
+def get_pet_labels(image_dir):
+    filename_list = listdir(image_dir)
+    results_dic = {}
+
+    for filename in os.listdir(image_dir):
+        if not filename.startswith('.'):
+            pet_label = os.path.splitext(filename)[0]
+            pet_label = pet_label.replace('_', ' ')
+            
+            # Extracting alphabetic characters and spaces
+            pet_name = ''
+            for c in pet_label:
+                if c.isalpha() or c.isspace():
+                    pet_name += c.lower()
+            
+            pet_name = pet_name.strip()
+            
+            if filename not in results_dic:
+                results_dic[filename] = [pet_name]
+            else:
+                print("** Warning: Duplicate files exist in directory:", filename)
+     
+    # Replace None with the results_dic dictionary that you created with this
+    # function
+    return results_dic
